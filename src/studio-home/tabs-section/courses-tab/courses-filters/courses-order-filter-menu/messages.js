d54c62e0aa06de2b7154342edae693ed61972977
m1ssha <m1ssha@vk.com>
2024-08-26 16:10:06 +0300

    переведено

--- src/studio-home/tabs-section/courses-tab/courses-filters/courses-order-filter-menu/messages.js
+++ src/studio-home/tabs-section/courses-tab/courses-filters/courses-order-filter-menu/messages.js
@@ -3,19 +3,19 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   coursesOrderFilterMenuAscendantCurses: {
     id: 'course-authoring.studio-home.courses.tab.order-filter-menu.ascendant-courses',
-    defaultMessage: 'Name A-Z',
+    defaultMessage: 'Название А-Я',
   },
   coursesOrderFilterMenuDescendantCurses: {
     id: 'course-authoring.studio-home.courses.tab.order-filter-menu.descendant-courses',
-    defaultMessage: 'Name Z-A',
+    defaultMessage: 'Название Я-А',
   },
   coursesOrderFilterMenuNewestCurses: {
     id: 'course-authoring.studio-home.courses.tab.order-filter-menu.newest-courses',
-    defaultMessage: 'Newest',
+    defaultMessage: 'Новые',
   },
   coursesOrderFilterMenuOldestCurses: {
     id: 'course-authoring.studio-home.courses.tab.order-filter-menu.oldest-courses',
-    defaultMessage: 'Oldest',
+    defaultMessage: 'Старые',
   },
 });
 
