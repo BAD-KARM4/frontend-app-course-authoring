55268ce312cff7797887e1e72d0ac6df847ff4a8
m1ssha <m1ssha@vk.com>
2024-08-26 22:11:50 +0300

    перевод

--- src/group-configurations/empty-placeholder/messages.js
+++ src/group-configurations/empty-placeholder/messages.js
@@ -3,22 +3,22 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   title: {
     id: 'course-authoring.group-configurations.empty-placeholder.title',
-    defaultMessage: 'You have not created any content groups yet.',
+    defaultMessage: 'Вы еще не создали никаких групп контента.',
     description: 'Title displayed when there are no content groups created yet.',
   },
   experimentalTitle: {
     id: 'course-authoring.group-configurations.experimental-empty-placeholder.title',
-    defaultMessage: 'You have not created any group configurations yet.',
+    defaultMessage: 'Вы еще не создали никаких групповых конфигураций.',
     description: 'Title displayed when there are no experimental group configurations created yet.',
   },
   button: {
     id: 'course-authoring.group-configurations.empty-placeholder.button',
-    defaultMessage: 'Add your first content group',
+    defaultMessage: 'Добавьте свою первую группу контента',
     description: 'Label for the button to add the first content group when none exist.',
   },
   experimentalButton: {
     id: 'course-authoring.group-configurations.experimental-empty-placeholder.button',
-    defaultMessage: 'Add your first group configuration',
+    defaultMessage: 'Добавьте свою первую групповую конфигурацию',
     description: 'Label for the button to add the first experimental group configuration when none exist.',
   },
 });
