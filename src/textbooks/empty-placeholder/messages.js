1634ebdbcf355a14165e9cd86d67d6f3e9615b5f
m1ssha <m1ssha@vk.com>
2024-08-27 16:59:47 +0300

    переведено

--- src/textbooks/empty-placeholder/messages.js
+++ src/textbooks/empty-placeholder/messages.js
@@ -1,12 +1,12 @@
 const descriptions = {
   title: {
     id: 'course-authoring.textbooks.empty-placeholder.title',
-    defaultMessage: 'You haven\'t added any textbooks to this course yet.',
+    defaultMessage: 'Не загружено никаких материалов',
     description: 'Message displayed when no textbooks are added to the course',
   },
   button: {
     id: 'course-authoring.textbooks.empty-placeholder.button.new-textbook',
-    defaultMessage: 'Add your first textbook',
+    defaultMessage: 'Добавьте материал',
     description: 'Text for the button to add the first textbook to the course',
   },
 };
