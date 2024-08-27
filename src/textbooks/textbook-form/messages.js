668767756d24490b5da33de35d5c89a6b31eb38d
m1ssha <m1ssha@vk.com>
2024-08-27 16:58:56 +0300

    переведено

--- src/textbooks/textbook-form/messages.js
+++ src/textbooks/textbook-form/messages.js
@@ -1,77 +1,77 @@
 const descriptions = {
   tabTitleLabel: {
     id: 'course-authoring.textbooks.form.tab-title.label',
-    defaultMessage: 'Textbook name',
+    defaultMessage: 'Название учебника',
     description: 'Label for the textbook name field in the form',
   },
   tabTitlePlaceholder: {
     id: 'course-authoring.textbooks.form.tab-title.placeholder',
-    defaultMessage: 'Introduction to Cookie Baking',
+    defaultMessage: 'Криптография',
     description: 'Placeholder text for the textbook name field in the form',
   },
   tabTitleHelperText: {
     id: 'course-authoring.textbooks.form.tab-title.helper-text',
-    defaultMessage: 'provide the title/name of the textbook as you would like your students to see it',
+    defaultMessage: 'укажите название учебника так, как вы хотели бы, чтобы его увидели ваши ученики',
     description: 'Helper text for the textbook name field in the form',
   },
   tabTitleValidationText: {
     id: 'course-authoring.textbooks.form.tab-title.validation-text',
-    defaultMessage: 'Textbook name is required',
+    defaultMessage: 'Обязательное поле',
     description: 'Validation error message for the textbook name field in the form',
   },
   chapterTitleLabel: {
     id: 'course-authoring.textbooks.form.chapter.title.label',
-    defaultMessage: 'Chapter name',
+    defaultMessage: 'Название главы',
     description: 'Label for the chapter name field in the form',
   },
   chapterTitlePlaceholder: {
     id: 'course-authoring.textbooks.form.chapter.title.placeholder',
-    defaultMessage: 'Chapter {value}',
+    defaultMessage: 'Глава {value}',
     description: 'Placeholder text for the chapter name field in the form',
   },
   chapterTitleHelperText: {
     id: 'course-authoring.textbooks.form.chapter.title.helper-text',
-    defaultMessage: 'provide the title/name of the chapter that will be used in navigating',
+    defaultMessage: 'укажите заголовок, который будет использоваться при навигации',
     description: 'Helper text for the chapter name field in the form',
   },
   chapterTitleValidationText: {
     id: 'course-authoring.textbooks.form.chapter.title.validation-text',
-    defaultMessage: 'Chapter name is required',
+    defaultMessage: 'Обязательное поле',
     description: 'Validation error message for the chapter name field in the form',
   },
   chapterUrlLabel: {
     id: 'course-authoring.textbooks.form.chapter.url.label',
-    defaultMessage: 'Chapter asset',
+    defaultMessage: 'Путь',
     description: 'Label for the chapter asset field in the form',
   },
   chapterUrlPlaceholder: {
     id: 'course-authoring.textbooks.form.chapter.url.placeholder',
-    defaultMessage: 'path/to/introductionToCookieBaking-CH1.pdf',
+    defaultMessage: 'path/to/cryptography.pdf',
     description: 'Placeholder text for the chapter asset field in the form',
   },
   chapterUrlHelperText: {
     id: 'course-authoring.textbooks.form.chapter.url.helper-text',
-    defaultMessage: 'upload a PDF file or provide the path to a Studio asset file',
+    defaultMessage: 'загрузите PDF-файл или укажите путь к файлу в PT EdTechLab',
     description: 'Helper text for the chapter asset field in the form',
   },
   chapterUrlValidationText: {
     id: 'course-authoring.textbooks.form.chapter.url.validation-text',
-    defaultMessage: 'Chapter asset is required',
+    defaultMessage: 'Обязательное поле',
     description: 'Validation error message for the chapter asset field in the form',
   },
   addChapterHelperText: {
     id: 'course-authoring.textbooks.form.add-chapter.helper-text',
-    defaultMessage: 'Please add at least one chapter',
+    defaultMessage: 'Пожалуйста, добавьте хотя бы одну главу',
     description: 'Helper text for adding a new chapter in the form',
   },
   addChapterButton: {
     id: 'course-authoring.textbooks.form.add-chapter.button',
-    defaultMessage: 'Add a chapter',
+    defaultMessage: 'Добавить главу',
     description: 'Text for the button to add a new chapter in the form',
   },
   uploadButtonTooltip: {
     id: 'course-authoring.textbooks.form.upload-button.tooltip',
-    defaultMessage: 'Upload',
+    defaultMessage: 'Загрузить',
     description: 'Tooltip text for the upload button in the form',
   },
   uploadButtonAlt: {
@@ -81,7 +81,7 @@ const descriptions = {
   },
   deleteButtonTooltip: {
     id: 'course-authoring.textbooks.form.delete-button.tooltip',
-    defaultMessage: 'Delete',
+    defaultMessage: 'Удалить',
     description: 'Tooltip text for the delete button in the form',
   },
   deleteButtonAlt: {
@@ -91,32 +91,32 @@ const descriptions = {
   },
   cancelButton: {
     id: 'course-authoring.textbooks.form.button.cancel',
-    defaultMessage: 'Cancel',
+    defaultMessage: 'Отменить',
     description: 'Text for the cancel button in the form',
   },
   saveButton: {
     id: 'course-authoring.textbooks.form.button.save',
-    defaultMessage: 'Save',
+    defaultMessage: 'Сохранить',
     description: 'Text for the save button in the form',
   },
   uploadModalTitle: {
     id: 'course-authoring.textbooks.form.upload-modal.title',
-    defaultMessage: 'Upload a new PDF to “{courseName}”',
+    defaultMessage: 'Загрузите новый PDF-файл в “{courseName}”',
     description: 'Title for the upload modal in the form',
   },
   uploadModalDropzoneText: {
     id: 'course-authoring.textbooks.form.upload-modal.dropzone-text',
-    defaultMessage: 'Drag and drop your PDF file here or click to upload',
+    defaultMessage: 'Перетащите ваш PDF-файл сюда или нажмите, чтобы загрузить',
     description: 'Text for the dropzone in the upload modal',
   },
   uploadModalHelperText: {
     id: 'course-authoring.textbooks.form.upload-modal.help-text',
-    defaultMessage: 'File must be in PDF format',
+    defaultMessage: 'Файл должен быть в формате PDF',
     description: 'Helper text for the upload modal',
   },
   uploadModalFileInvalidSizeText: {
     id: 'course-authoring.textbooks.form.upload-modal.file-size-invalid-text',
-    defaultMessage: 'File size must be less than {maxSize}MB.',
+    defaultMessage: 'Размер файла должен быть меньше {maxSize} МБ.',
     description: 'Error message for invalid file size in the upload modal',
   },
 };
