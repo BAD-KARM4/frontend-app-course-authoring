37106f9d1e89fb380a6a290f2a7838e529e0911f
m1ssha <m1ssha@vk.com>
2024-08-27 16:53:45 +0300

    удален helpSidebar

--- src/textbooks/textbook-sidebar/TextbookSidebar.jsx
+++ src/textbooks/textbook-sidebar/TextbookSidebar.jsx
@@ -13,19 +13,6 @@ const TextbookSidebar = ({ courseId }) => {
 
   return (
     <HelpSidebar courseId={courseId} className="pt-4">
-      <h4 className="help-sidebar-about-title">
-        {intl.formatMessage(messages.section_1_title)}
-      </h4>
-      <p className="help-sidebar-about-descriptions">
-        {intl.formatMessage(messages.section_1_descriptions)}
-      </p>
-      <hr className="my-3.5" />
-      <h4 className="help-sidebar-about-title">
-        {intl.formatMessage(messages.section_2_title)}
-      </h4>
-      <p className="help-sidebar-about-descriptions">
-        {intl.formatMessage(messages.section_2_descriptions)}
-      </p>
     </HelpSidebar>
   );
 };
